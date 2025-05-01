5c1980a08e0cdff60c058ea8459f1d3659d8db43
宋九傲 <jiuaosong@BP.local>
2025-05-01 15:40:14 -0400

    init

---
+++ deck/src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+fn main() {
+    let suits= vec!["Hearts", "Diamonds", "Clubs", "Spades"];
+    let values = ["Ace", "Two", "Three"];
+    let mut cards = vec![];
+    for suit in suits
+    {
+        for value in values{
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+    let deck = Deck { cards };
+    println!("Heres your deck: {:#?}" , deck);
+}
